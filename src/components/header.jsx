8bfd130fb0e261d6b9732cdab85fad64ef7a4d29
VEPO <147517333+JEM-YORONG@users.noreply.github.com>
2024-04-26 09:43:27 +0800

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Projects</a>
+          </li>
+          <li>
+            <a href="/">contact</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
+export default Header;
